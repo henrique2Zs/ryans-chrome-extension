18aa043fe39d74b0add8fa836116a8f1b3505883
Jacklyn Biggin <jacklyn@jmb.im>
2021-07-04 19:47:42 -0400

    more

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
-function reddenPage() {
-    document.body.style.backgroundColor = 'red';
-    console.log("this!")
-    let h1 = document.getElementsByTagName("h1")
-    for (let i = 0; i < h1.length; i++) {
-        console.log("this is h1!")
-        console.log(h1[i])
-        h1[i].innerHTML = "💩"
-        
+function poopifyRyan() {
+    let re = /((R|r)yan)/g;
+    let re2 = /((S|s)wift)/g;
+    let element = document.querySelectorAll('i,b,p,span,div,li,h1,h2,h3,h4,h5,h6')
+    for (let i = 0; i < element.length; i++) {
+        element[i].innerHTML = element[i].innerHTML.replace(re, "💩")    
+        element[i].innerHTML = element[i].innerHTML.replace(re2, "🦈")    
     }
   }
   
   chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage
+      function: poopifyRyan
     });
   });
+  
   
